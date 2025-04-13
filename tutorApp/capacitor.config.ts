978e5d66cc4f8fd76d913c82a4e2a97f06c05d8b
Johnson <G00419319@atu.ie>
2025-04-13 18:12:15 +0100

    finished setup

---
+++ tutorApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tutorApp',
+  webDir: 'www'
+};
+
+export default config;
